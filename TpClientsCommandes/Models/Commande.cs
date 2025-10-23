328b63ce5b242ef84f10ed7849aae9b24a35b23b
Artillio7 <jtchotcheuyepmo@gmail.com>
2025-10-23 10:02:45 +0200

    feat(validation): DataAnnotations + FluentValidation (validators + intégration Program.cs)

--- TpClientsCommandes/Models/Commande.cs
+++ TpClientsCommandes/Models/Commande.cs
@@ -1,16 +1,20 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TpClientsCommandes.Models
 {
     public class Commande
     {
         public int Id { get; set; }
-        public string NumeroCommande { get; set; }
+        [Required, StringLength(50)]
+        public string NumeroCommande { get; set; } = default!;
         public DateTime DateCommande { get; set; }
+        [Range(typeof(decimal), "0.01", "79228162514264337593543950335")]
         public decimal MontantTotal { get; set; }
-        public string Statut { get; set; }
+        [Required, StringLength(50)]
+        public string Statut { get; set; } = default!;
 
         public int ClientId { get; set; }
-        public Client Client { get; set; }
+        public Client Client { get; set; } = default!;
     }
 }
--- TpClientsCommandes/Program.cs
+++ TpClientsCommandes/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using TpClientsCommandes.Data;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Controllers & FluentValidation
+builder.Services.AddControllers();
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<TpClientsCommandes.Validators.ClientValidator>();
 
 var app = builder.Build();
 
@@ -20,6 +27,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Map API controllers
+app.MapControllers();
 
 var summaries = new[]
 {
